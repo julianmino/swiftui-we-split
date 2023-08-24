305e218aa3c5bcce1f1986cea5121b2fe7b3ca73
Julián Miño <julian.mino@possumus.tech>
2023-08-24 17:50:18 -0300

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Julian Miño on 24/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
